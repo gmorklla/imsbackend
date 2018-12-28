1393c7d181d7452d5aa461096027a1d68611f44d
gmorklla <gmorklla@gmail.com>
2018-12-27 19:51:06 -0600

    Change: DataLoadSchema to save nedns like array by time;

--- db/models/dataLoadModel.js
+++ db/models/dataLoadModel.js
@@ -3,40 +3,46 @@ const Schema = mongoose.Schema;
 
 // Parser Schema
 const DataLoadSchema = new Schema({
-    documentKey: {
+  documentKey: {
+    type: String,
+    required: true
+  },
+  formulaName: {
+    type: String,
+    required: true
+  },
+  day: {
+    type: Date,
+    required: true
+  },
+  measurement: {
+    type: String,
+    required: true
+  },
+  data: [
+    {
+      time: {
         type: String,
         required: true
-    },
-    formulaName: {
-        type: String,
-        required: true
-    },
-    day: {
-        type: Date,
-        required: true
-    },
-    measurement: {
-        type: String,
-        required: true,
-    },
-    nedn: [{
-        name: {
-            type: String,
-            required: true
-        },
-        values: []
-    }]
+      },
+      nedns: []
+    }
+  ]
 });
 
-DataLoadSchema.index({
-    "day": -1,
-    "formulaName": 1,
-    "measurement": 1,
-    "nedn.name": 1
-}, {
+DataLoadSchema.index(
+  {
+    documentKey: 1,
+    day: -1,
+    formulaName: 1,
+    measurement: 1,
+    'nedn.name': 1
+  },
+  {
     background: true,
     unique: true
-});
+  }
+);
 
 // Issue model
 const DataLoad = mongoose.model('DataLoad', DataLoadSchema);
--- utilities/changeStream.js
+++ utilities/changeStream.js
@@ -1,181 +1,225 @@
 const ParserW = require('../db/models/parserModel');
 const DataLoad = require('../db/models/dataLoadModel');
 
-const formulas = [{
-        name: 'IMSCSCFInitRegSuccRatio',
-        counters: [
-            'cscfAcceptedRegistrations.DEFAULT',
-            'cscfRegistrationsFailure.SIPResponseCode=400',
-            'cscfRegistrationsFailure.SIPResponseCode=401',
-            'cscfRegistrationsFailure.SIPResponseCode=403',
-            'cscfRegistrationsFailure.SUM'
-        ]
-    },
-    {
-        name: 'IMSCSCFOrgSessSetupSuccRatio',
-        counters: [
-            'scscfOriginatingInviteSuccessfulEstablishedNoAs.sum',
-            'scscfOriginatingInviteSuccessfulEstablishedToAs.sum',
-            'scscfOriginatingInviteCancelledBeforeEarlyDialog.DEFAULT',
-            'scscfOriginatingInviteNoAsFailed.403',
-            'scscfOriginatingInviteNoAsFailed.407',
-            'scscfOriginatingInviteNoAsFailed.484',
-            'scscfOriginatingInviteToAsFailed.403',
-            'scscfOriginatingInviteToAsFailed.407',
-            'scscfOriginatingInviteToAsFailed.484',
-            'scscfOriginatingInviteNoAsAttempts.DEFAULT',
-            'cscfOriginatingInviteToAsAttempts.DEFAULT'
-        ]
-    }
+const formulas = [
+  {
+    name: 'IMSCSCFInitRegSuccRatio',
+    counters: [
+      'cscfAcceptedRegistrations.DEFAULT',
+      'cscfRegistrationsFailure.SIPResponseCode=400',
+      'cscfRegistrationsFailure.SIPResponseCode=401',
+      'cscfRegistrationsFailure.SIPResponseCode=403',
+      'cscfRegistrationsFailure.SUM'
+    ]
+  },
+  {
+    name: 'IMSCSCFOrgSessSetupSuccRatio',
+    counters: [
+      'scscfOriginatingInviteSuccessfulEstablishedNoAs.sum',
+      'scscfOriginatingInviteSuccessfulEstablishedToAs.sum',
+      'scscfOriginatingInviteCancelledBeforeEarlyDialog.DEFAULT',
+      'scscfOriginatingInviteNoAsFailed.403',
+      'scscfOriginatingInviteNoAsFailed.407',
+      'scscfOriginatingInviteNoAsFailed.484',
+      'scscfOriginatingInviteToAsFailed.403',
+      'scscfOriginatingInviteToAsFailed.407',
+      'scscfOriginatingInviteToAsFailed.484',
+      'scscfOriginatingInviteNoAsAttempts.DEFAULT',
+      'cscfOriginatingInviteToAsAttempts.DEFAULT'
+    ]
+  }
 ];
 
-const filter = [{
-        $match: {
-            $or: [{
-                    $and: [{
-                            'fullDocument.measurement': {
-                                $eq: 'cscfAcceptedRegistrations'
-                            }
-                        },
-                        {
-                            'fullDocument.moid': {
-                                $eq: 'DEFAULT'
-                            }
-                        }
-                    ]
-                },
-                {
-                    $and: [{
-                            'fullDocument.measurement': {
-                                $eq: 'cscfRegistrationsFailure'
-                            }
-                        },
-                        {
-                            'fullDocument.moid': {
-                                $in: [
-                                    'SIPResponseCode=400',
-                                    'SIPResponseCode=401',
-                                    'SIPResponseCode=403',
-                                    'SUM'
-                                ]
-                            }
-                        }
-                    ]
-                },
-                {
-                    operationType: {
-                        $eq: 'update'
-                    }
-                }
-            ]
+const filter = [
+  {
+    $match: {
+      $or: [
+        {
+          $and: [
+            {
+              'fullDocument.measurement': {
+                $eq: 'cscfAcceptedRegistrations'
+              }
+            },
+            {
+              'fullDocument.moid': {
+                $eq: 'DEFAULT'
+              }
+            }
+          ]
+        },
+        {
+          $and: [
+            {
+              'fullDocument.measurement': {
+                $eq: 'cscfRegistrationsFailure'
+              }
+            },
+            {
+              'fullDocument.moid': {
+                $in: [
+                  'SIPResponseCode=400',
+                  'SIPResponseCode=401',
+                  'SIPResponseCode=403',
+                  'SUM'
+                ]
+              }
+            }
+          ]
+        },
+        {
+          operationType: {
+            $eq: 'update'
+          }
         }
-    },
-    {
-        $project: {
-            ns: 0,
-            'fullDocument._id': 0
-        }
+      ]
     }
+  },
+  {
+    $project: {
+      ns: 0,
+      'fullDocument._id': 0
+    }
+  }
 ];
 
 const keys = [];
 
 ParserW.watch(filter, {
-    fullDocument: 'updateLookup'
+  fullDocument: 'updateLookup'
 }).on('change', data => {
-    if (data.operationType === 'insert') {
-        keys.push(String(data.documentKey._id));
-        // ** Check counters that are used on several formulas option
-        const name = getFormulaName(data);
-        saveInsertDataLoad(data, name);
+  if (data.operationType === 'insert') {
+    keys.push(String(data.documentKey._id));
+    // ** Check counters that are used on several formulas option
+    const name = getFormulaName(data);
+    saveInsertDataLoad(data, name)
+      .then(_ => {})
+      .catch(err => console.log('error saveInsert', err));
+  }
+  if (data.operationType === 'update') {
+    if (keys.includes(String(data.documentKey._id))) {
+      const name = getFormulaName(data);
+      saveUpdateDataLoad(data, name)
+        .then(val => console.log('saveUpdate', val))
+        .catch(err => console.log('error saveUpdate', err));
     }
-    if (data.operationType === 'update') {
-        if (keys.includes(String(data.documentKey._id))) {
-            saveUpdateDataLoad(data);
-        }
-    }
+  }
 });
 
 function getFormulaName(doc) {
-    const {
-        fullDocument: {
-            measurement,
-            moid
-        }
-    } = doc;
-    const counter = measurement + '.' + moid;
-    let fName = '';
-    formulas.forEach(formula => {
-        const check = formula.counters.includes(counter);
-        if (check) {
-            fName = formula.name;
-        }
-    });
-    return fName;
+  const {
+    fullDocument: { measurement, moid }
+  } = doc;
+  const counter = measurement + '.' + moid;
+  let fName = '';
+  formulas.forEach(formula => {
+    const check = formula.counters.includes(counter);
+    if (check) {
+      fName = formula.name;
+    }
+  });
+  return fName;
 }
 
-function saveInsertDataLoad(doc, fName) {
-    const {
-        fullDocument: {
-            day,
-            measurement,
-            moid,
-            nedn,
-            values
-        },
-        documentKey: {
-            _id
-        }
-    } = doc;
-    const compMeasurement = measurement + '.' + moid;
-    const hour = Object.keys(values)[0];
-    const minutes = Object.keys(values[hour])[0];
-    const step = hour + '.' + minutes;
-    const obj = {
-        documentKey: _id,
+async function saveInsertDataLoad(doc, fName) {
+  const {
+    fullDocument: { day, measurement, moid, nedn, values },
+    documentKey: { _id }
+  } = doc;
+  const compMeasurement = measurement + '.' + moid;
+  const hour = Object.keys(values)[0];
+  const minutes = Object.keys(values[hour])[0];
+  const step = hour + '.' + minutes;
+  const obj = {
+    documentKey: String(_id),
+    formulaName: fName,
+    day: day,
+    measurement: compMeasurement,
+    data: [
+      {
+        time: step,
+        nedns: [nedn]
+      }
+    ]
+  };
+  try {
+    const inDb = await DataLoad.findOne(
+      {
         formulaName: fName,
         day: day,
         measurement: compMeasurement,
-        nedn: {
-            name: nedn,
-            values: step
-        }
-    };
-    DataLoad.create(obj)
-        .then(val => console.log('saveInsertDataLoad done!', val))
-        .catch(err => console.log('Err', err));
+        'data.time': step
+      },
+      {
+        _id: 1
+      }
+    );
+    if (inDb) {
+      return await updateData(inDb, nedn, step);
+    } else {
+      return await createNewData(obj);
+    }
+  } catch (error) {
+    console.log('Error on saveInsertDataLoad', error);
+  }
 }
 
-function saveUpdateDataLoad(doc) {
-    console.log('saveUpdate doc', doc);
-    const {
-        fullDocument: {
-            nedn
-        },
-        updateDescription: {
-            updatedFields
-        },
-        documentKey: {
-            _id
-        }
-    } = doc;
-    console.log('updatedFields', updatedFields);
-    const pieces = Object.keys(updatedFields)[0].split('.');
-    console.log('pieces', pieces);
-    const val = pieces[1] + '.' + pieces[2];
-    console.log('val', val);
-    DataLoad.findOneAndUpdate({
-            documentKey: _id,
-            'nedn.name': nedn
-        }, {
-            $push: {
-                'nedn.$.values': val
-            }
-        }, {
-            new: true
-        })
-        .then(val => console.log('saveUpdateDataLoad done!', val))
-        .catch(err => console.log('Err', err));
+function createNewData(obj) {
+  return DataLoad.create(obj)
+    .then(val => val)
+    .catch(err => err);
+}
+
+function updateData(id, nedn, step) {
+  console.log('//////////// updateData', id, nedn, step);
+  return DataLoad.findOneAndUpdate(
+    { _id: id, 'data.time': step },
+    { $push: { 'data.$.nedns': nedn } }
+  )
+    .then(val => val)
+    .catch(err => err);
+}
+
+async function saveUpdateDataLoad(doc, fName) {
+  const {
+    fullDocument: { day, measurement, moid, nedn },
+    updateDescription: { updatedFields },
+    documentKey: { _id }
+  } = doc;
+  const compMeasurement = measurement + '.' + moid;
+  const pieces = Object.keys(updatedFields)[0].split('.');
+  const step = pieces[1] + '.' + pieces[2];
+  const inDb = await DataLoad.findOne(
+    {
+      formulaName: fName,
+      day: day,
+      measurement: compMeasurement,
+      'data.time': step
+    },
+    {
+      _id: 1
+    }
+  );
+  if (inDb) {
+    console.log('//////////// inDb update', inDb);
+    return await updateData(inDb, nedn, step);
+  } else {
+    const obj = {
+      time: step,
+      nedns: [nedn]
+    };
+    return await createNewObjInDataArr(obj, _id);
+  }
+}
+
+function createNewObjInDataArr(obj, id) {
+  return DataLoad.findOneAndUpdate(
+    { documentKey: id },
+    { $push: { data: obj } },
+    { new: true }
+  )
+    .then(val => val)
+    .catch(err => err);
 }
 
 module.exports = ParserW;
